b4890550eafdd0c98cddbf94029ab6ca26484ced
aalkholeify <amal.al.kholeify@gmail.com>
2018-04-21 15:09:09 +0300

    updates

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('list-view/', views.area_list, name='list-view'),
     path('category/taskers/<int:category_id>/', views.category_detail, name='category_detail'),
+    path('main/category/taskers/profile/<int:tasker_id>/', views.tasker_profile_detail, name='tasker_profile_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,7 +2,22 @@ from .models import Category
 from users.models import Tasker
 from django.shortcuts import render, redirect
 from django.utils import timezone
+from main.models import Area
+from django.db.models import Q
 
+def area_list(request):
+    areas = Area.objects.all()
+    query = request.GET.get("q")
+    if query:
+        areas = areas.filter(
+            Q(name__icontains=query)
+            
+            ).distinct()
+
+    context = {
+        "area": areas,
+    }
+    return render(request, 'home.html', context)
 def home(request):
 	context = {
 		"category_list": Category.objects.all(),
@@ -18,4 +33,16 @@ def category_detail(request, category_id):
 	context = {
 		"taskers": taskers
 	}
+	return render(request, 'tasker_profile_detail.html', context)
+
+def tasker_profile_detail(request, tasker_id):
+	tasker_profile = Category.objects.get(id=tasker_id)
+	tasker = Tasker.objects.get(id=tasker_id)
+	profiles = tasker_profile.tasker_set.all()
+
+	context = {
+		"profiles": profiles,
+		"tasker": tasker,
+	}
 	return render(request, 'category_detail.html', context)
+
