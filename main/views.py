2b6aa14cef8a80f27970daf6e65b926236b6bf4a
DarthHamza <hamzamakia@gmail.com>
2018-04-28 11:43:51 +0300

    fixed pagination

--- main/views.py
+++ main/views.py
@@ -9,59 +9,50 @@ from users.models import Tasker
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
-	form = TaskerSearch()
-	taskers = Tasker.objects.none()
-	categories = request.GET.get('categories')
-	areas = request.GET.get('areas')
-	if categories and areas:
-		taskers = Tasker.objects.filter(
-			categories__id=categories,
-			areas__id=areas
-			)
-	context = {
-		"taskers": taskers,
-		"categories": categories,
-		"areas": areas,
-		"form": form,
-	}
-	return render(request, 'home.html', context)
+    form = TaskerSearch()
+    taskers = Tasker.objects.none()
+    categories = request.GET.get('categories')
+    areas = request.GET.get('areas')
+    if categories and areas:
+        taskers = Tasker.objects.filter(
+            categories__id=categories,
+            areas__id=areas
+            )
+    context = {
+        "taskers": taskers,
+        "categories": categories,
+        "areas": areas,
+        "form": form,
+    }
+    return render(request, 'home.html', context)
 
 def list(request):
-	form = TaskerSearch()
-	taskers = Tasker.objects.none()
-	categories = request.GET.get('categories')
-	areas = request.GET.get('areas')
-	if categories and areas:
-		taskers = Tasker.objects.filter(
-			categories__id=categories,
-			areas__id=areas
-			)
-	paginator = Paginator(categories, 3) 
-	page = request.GET.get('page')
-	try:
-		objects = paginator.page(page)
+    form = TaskerSearch()
+    taskers = Tasker.objects.none()
+    categories = request.GET.get('categories')
+    areas = request.GET.get('areas')
+    if categories and areas:
+        taskers = Tasker.objects.filter(
+            categories__id=categories,
+            areas__id=areas
+            )
 
-	except PageNotAnInteger:
-		objects = paginator.page(1)
-	except EmptyPage:
-		objects = paginator.page(paginator.num_pages)
-		
-	context = {
-		"taskers": taskers,
-		"categories": categories,
-		"areas": areas,
-		"form": form,
-		"categories": objects,
-
-	}
-	return render(request, 'list.html', context)
+    paginator = Paginator(taskers, 5)
+    page = request.GET.get('page')
+    tasker_list = paginator.get_page(page)
+        
+    context = {
+        "taskers": tasker_list,
+        "form": form,
+    }
+    return render(request, 'list.html', context)
 
 def about(request):
-	return render(request, 'about.html')
+    return render(request, 'about.html')
 
 def tasker_profile_detail(request, tasker_id):
-	context = {
-			"user": Tasker.objects.get(id=tasker_id)
-		}
-	return render(request, 'tasker_profile_detail.html', context)
+    context = {
+            "user": Tasker.objects.get(id=tasker_id)
+        }
+    return render(request, 'tasker_profile_detail.html', context)
 
