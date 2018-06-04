c826d94964954dd4484ae608ee7102d16b3c64b4
aalkholeify <amal.al.kholeify@gmail.com>
2018-06-04 09:46:10 +0300

    adjus

--- api/urls.py
+++ api/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import UserCreateAPIView,UserLoginAPIView ,TaskerCreateAPIView,TaskerLoginAPIView,Task_RequestAPIView,Task_RequestListView, Task_RequestDetailView,Task_RequestUpdateView 
 
 urlpatterns = [
-	path('signup/', UserCreateAPIView.as_view(), name='signup'),
-	path('signin/', UserLoginAPIView.as_view(), name='signin'),
+	path('signup/', UserCreateAPIView.as_view(), name='signupapi'),
+	path('signin/', UserLoginAPIView.as_view(), name='signinapi'),
 	path('taskersignin/', TaskerLoginAPIView.as_view(), name='taskersignin'),
 	path('taskersignup/', TaskerCreateAPIView.as_view(), name='taskersignup'),
-	path('task/', Task_RequestAPIView.as_view(), name='task'),
-	path('list/', Task_RequestListView.as_view(), name='list'),
-	path('detail/<int:object_id>/', Task_RequestDetailView.as_view(), name='detail'),
-	path('update/<int:object_id>/', Task_RequestUpdateView.as_view(), name='update'),
+	path('task/', Task_RequestAPIView.as_view(), name='apitask'),
+	path('list/', Task_RequestListView.as_view(), name='apilist'),
+	path('detail/<int:object_id>/', Task_RequestDetailView.as_view(), name='apidetail'),
+	path('update/<int:object_id>/', Task_RequestUpdateView.as_view(), name='apiupdate'),
 
 ]
--- users/models.py
+++ users/models.py
@@ -39,3 +39,23 @@ class Task_Request(models.Model):
 
 	def get_absolute_url(self):
 		return "request/%d/view" % self.pk
+
+class Notification(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE ,null=True, blank=True)
+	tasker = models.ForeignKey(Tasker,on_delete=models.CASCADE ,null=True, blank=True)
+	date = models.DateTimeField(default=timezone.now)
+	time = models.DateTimeField(blank=True, null=True)
+	contact_number = models.PositiveIntegerField(null=True, blank=True)
+	description = models.TextField()
+	TASK_CHOICES = (
+        ('Approved', 'Approved'),
+        ('Denied', 'Denied'),
+        ('Pending', 'Pending'),
+    )
+	status = models.CharField(max_length=20, choices=TASK_CHOICES, default='Pending')
+	
+	def __str__(self):
+		return '{}'.format(self.user)
+
+	def get_absolute_url(self):
+		return "request/%d/view" % self.pk
