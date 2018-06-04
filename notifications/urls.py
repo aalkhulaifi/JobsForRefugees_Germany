057bc23a36a7c952393d28953f238b529e126691
aalkholeify <amal.al.kholeify@gmail.com>
2018-06-04 04:13:37 +0300

    initial notification

--- notifications/urls.py
+++ notifications/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sent_task/<int:pk>/', views.sent_task, name= 'request'),
-    path('accept/<int:request_id>/', views.accepted_request, name = 'accept' ),
-    path('deni/<int:request_id>/', views.deni_request, name = 'deni' ),
+    # path('sent_task/<int:pk>/', views.sent_task, name= 'request'),
+    # path('accept/<int:request_id>/', views.accepted_request, name = 'accept' ),
+    # path('deni/<int:request_id>/', views.deni_request, name = 'deni' ),
 
 ]
